765bca0538f089c2c7c8cff8c50a1e6681342b75
vignesh <vigneshchinnadurai467@gmail.com>
2025-11-17 09:53:08 +0530

    update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -75,7 +75,9 @@ const Footer = () => {
       
         <div class="py-7 border-t border-gray-200">
             <div class="flex items-center justify-center flex-col lg:justify-between lg:flex-row">
-                <span class="text-sm text-gray-500 ">©<a href="https://pagedone.io/"></a> 2024, All rights reserved.</span>
+                <span class="text-sm text-black ">©<a href="https://pagedone.io/"></a> 2025, All rights reserved.</span>
+                  <p className="text-center">Designed by <span className="text-[#00CD97]">Prognoza</span> <span className="text-[#336699]">Dataminds</span></p>
+             
                 <div class="flex mt-4 space-x-4 sm:justify-center lg:mt-0 ">
                     <a href="javascript:;"  class="w-9 h-9 rounded-full bg-gray-700 flex justify-center items-center hover:bg-[#336699]">
                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
